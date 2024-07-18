32c7c903bdcf2b75cc153d4283bf9a8ddf3b25b8
Henskelis <henskelis@outlook.com>
2024-07-18 23:46:22 +0200

    add documentation project and various code refactoring

---
+++ docs/docs/intro.md
@@ -0,0 +1,9 @@
+---
+slug: /
+sidebar_position: 1
+---
+
+# Documentation
+
+A framework agnostic UI library built on top of the [Web Component API](https://developer.mozilla.org/en-US/docs/Web/API/Web_components).\
+**Helium UI** is currently under active development.
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,76 @@
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
+
+const config: Config = {
+	title: "Helium UI",
+	tagline: "Dinosaurs are cool",
+	favicon: "img/favicon.ico",
+
+	// Set the production url of your site here
+	url: "https://helium-ui.henskelis.com",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/",
+
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "Henskelis", // Usually your GitHub org/user name.
+	projectName: "helium-ui", // Usually your repo name.
+
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en", "fr"],
+	},
+
+	presets: [
+		[
+			"classic",
+			{
+				docs: {
+					routeBasePath: "/",
+					sidebarPath: "./sidebars.ts",
+				},
+				blog: false,
+				theme: {
+					customCss: "./src/css/custom.css",
+				},
+			} satisfies Preset.Options,
+		],
+	],
+
+	themeConfig: {
+		// Replace with your project's social card
+		image: "img/docusaurus-social-card.jpg",
+		navbar: {
+			title: "Helium UI",
+			logo: {
+				alt: "Helium UI logo",
+				src: "img/logo.svg",
+			},
+			items: [
+				{
+					href: "https://github.com/Henskelis/helium-ui",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
+		footer: {
+			style: "light",
+			copyright: `Copyright © ${new Date().getFullYear()} Helium UI`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
+};
+
+export default config;
