9656537237df9dc2913366cf1919da9b757f32d6
Henskelis <henskelis@outlook.com>
2024-07-19 14:16:41 +0200

    add fade animations

---
+++ packages/animations/src/keyframes/fade-entrances.ts
@@ -0,0 +1,13 @@
+/**
+ * Entrance with a fading motion
+ */
+export const fadeEntrance: Keyframe[] = [
+  {
+    offset: 0,
+    opacity: "0",
+  },
+  {
+    offset: 1,
+    opacity: "1",
+  },
+];
---
+++ packages/animations/src/keyframes/fade-exits.ts
@@ -0,0 +1,13 @@
+/**
+ * Exit with a fading motion
+ */
+export const fadeExit: Keyframe[] = [
+  {
+    offset: 0,
+    opacity: "1",
+  },
+  {
+    offset: 1,
+    opacity: "0",
+  },
+];
